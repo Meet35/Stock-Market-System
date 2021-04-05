f035b4fba87cf84becd13eae54bfb3aac0a7336f
Brijesh <bjkhuntkanbi@gmail.com>
2021-04-05 11:21:45 +0530

    changes baseurl to heroku in frontend

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 //'https://stock-market-system.herokuapp.com/'
 //'http://localhost:5000'
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://stock-market-system.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
